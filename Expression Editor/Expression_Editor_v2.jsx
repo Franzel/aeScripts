25e708a9c8f01a355fe79b474b2adad810b94d42
Francisco Zamorano <franciscozamorano@francisco.local>
2015-07-22 19:02:20 -0400

    Implemented functionality to copy expressions to the edit text box, only at startup in the meantime, and crashes when expression is multiline

--- Expression Editor/Expression_Editor_v2.jsx
+++ Expression Editor/Expression_Editor_v2.jsx
@@ -1,12 +1,21 @@
 
 function expEditor(thisObj){
 
-	var project = app.project;
+	 this.project = app.project;
 
-	var selectedComp = project.activeItem;
-	var selectedCompIndex = getIndexfromName(selectedComp.name);	
-	var selectedLayers = project.item(selectedCompIndex).selectedLayers;
-	var selectedProperties = project.item(selectedCompIndex).selectedProperties;
+
+	if(app.project.activeItem !== null){
+		this.selectedComp = project.activeItem;
+		this.selectedCompIndex = getIndexfromName(selectedComp.name);	
+		this.selectedLayers = project.item(selectedCompIndex).selectedLayers;
+		this.selectedProperties = project.item(selectedCompIndex).selectedProperties;
+		this.currentExpression = this.selectedProperties[0].expression;
+	}else{
+		alert("Please select a Layer Property first");
+	}
+
+	
+
 
 	function bakeExpression(expression){
 		this.selectedProperty = selectedProperties[0];
@@ -27,16 +36,17 @@ function expEditor(thisObj){
 
 	this.buildUI = function(thisObj){
 
-		var pal =  (thisObj instanceof Panel) ? thisObj: new Window("palette", "Title", undefined, {resizable:true} );
+		alert(selectedProperties[0].expression);
+		var pal =  (thisObj instanceof Panel) ? thisObj: new Window("palette", "Expression Editor", undefined, {resizable:true} );
 
 		//resource specs
 		var res = 
 		"group { orientation:'column', alignment:['left', 'top'], alignChildren:['right', 'top'], \
 			gr_Actions: Group { \
-				runBtn: Button { text:'run', preferredSize:[25,20]} \
+				runBtn: Button { text:'Apply', preferredSize:[50,20]} \
 			}, \
 			gr_Editor: Group { \
-				editField : EditText {text:' 0', properties: {multiline:true}, enterKeySignalsOnChange:true, preferredSize:[100,200], scrollable:true, alignChildren:['top','left']} \
+				editField : EditText {text:'" + this.currentExpression.toString() + "', properties: {multiline:true}, enterKeySignalsOnChange:true, preferredSize:[250,500], scrollable:true, alignChildren:['top','left']} \
 			} \
 		}";
 
