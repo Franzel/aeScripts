5b3babb149a0104b3abf6b33baf793a428078414
Francisco Zamorano <franciscozamorano@francisco.local>
2015-07-23 10:36:56 -0400

    Fixed broken infoText, still buggy

--- Expression Editor/Expression_Editor_v2.jsx
+++ Expression Editor/Expression_Editor_v2.jsx
@@ -12,11 +12,13 @@ function expEditor(thisObj){
 			this.selectedProperties = project.item(selectedCompIndex).selectedProperties;
 			this.currentExpression = this.selectedProperties[0].expression;
 			this.selectedProperty = selectedProperties[0];
-	 		return this.selectedProperty
+	 		
 		}else{
 			alert("Please select a Layer Property first");
 			return " ";
 		}
+
+		return this.selectedProperty
 	 }
 
 
@@ -68,7 +70,7 @@ function expEditor(thisObj){
 				runBtn: Button { text:'Apply', preferredSize:[50,20]} \
 			}, \
 			gr_Editor: Group { orientation:'column', \
-				infoField : StaticText {text:'', alignment:['left','top']}, \
+				infoField : StaticText {text:'" + getSelectedProperty().name + "', alignment:['left','top']}, \
 				editField : EditText {text:'', properties: {multiline:true}, enterKeySignalsOnChange:true, preferredSize:[300,500], scrollable:true, alignChildren:['top','left']} \
 			} \
 		}";
