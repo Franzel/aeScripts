0dc34b7fcffd105881367ba1933760dde306b931
Francisco Zamorano <franciscozamorano@francisco.local>
2015-07-30 19:14:33 -0400

    Implemented getting to a Property Group (ie. Effect)

--- Expression Editor/Expression_Editor_v4.jsx
+++ Expression Editor/Expression_Editor_v4.jsx
@@ -3,6 +3,11 @@ function expEditor()
 {
 	var expEditor = this;
 
+	//messages
+	this.errNoValidProp = "This is not a valid property";
+	this.errNoSelectedProp = "Select a Property first";
+	this.errNoSelectedComp = "No Composition is selected"
+
 	this.buildUI = function (thisObj)
 	{
 		// dockable panel or palette
@@ -10,15 +15,15 @@ function expEditor()
 
 		//resource specs
 		var res = 
-		"group { orientation:'column', alignment:['left', 'top'], alignChildren:['right', 'top'], \
-			gr_Actions: Group { orientation:'row', \
+		"group { orientation:'column', alignment:['left', 'top'], alignChildren:['left', 'top'], \
+			gr_Actions: Group { orientation:'row', alignChildren:['left', 'top'],\
 				getBtn: Button { text:'Get', preferredSize:[50,20]}, \
 				clearBtn: Button { text:'Clear', preferredSize:[50,20]}, \
 				runBtn: Button { text:'Apply', preferredSize:[50,20]} \
 			}, \
 			gr_Editor: Group { orientation:'column', \
 				infoField : StaticText {text:'Current Property: ', alignment:['left','top'], characters: 30}, \
-				editField : EditText {text:' ', properties: {multiline:true}, enterKeySignalsOnChange:true, preferredSize:[300,500], scrollable:true, alignChildren:['top','left']} \
+				editField : EditText {text:' ', properties: {multiline:true}, enterKeySignalsOnChange:true, preferredSize:[600,500], scrollable:true, alignChildren:['top','left']} \
 			} \
 		}";
 		pal.grp = pal.add(res);
@@ -26,7 +31,6 @@ function expEditor()
 		// Callbacks
 		pal.grp.gr_Actions.getBtn.onClick = function ()
 		{
-			expEditor.getSelectedProperty();
 			pal.grp.gr_Editor.infoField.text = "Current Property: " + expEditor.getSelectedProperty().name;
 			pal.grp.gr_Editor.editField.text = expEditor.getExpression();
 		};
@@ -37,14 +41,12 @@ function expEditor()
 			expEditor.bakeExpression(pal.grp.gr_Editor.editField.text);
 		};
 
-		// pal.grp.gr_Actions.clearBtn.onClick = function(){
-		// 	var selectedProperty = getSelectedProperty(pal);
-		// 	app.beginUndoGroup("Clear Expression");
-		// 	selectedProperty.expression = "";
-		// 	pal.grp.gr_Editor.infoField.text = "Current Property: " + getSelectedProperty(pal).name;
-		// 	pal.grp.gr_Editor.editField.text = "";
-		// 	app.endUndoGroup();
-		// }
+		pal.grp.gr_Actions.clearBtn.onClick = function(){
+			app.beginUndoGroup("Clear Expression");
+			pal.grp.gr_Editor.infoField.text = "Current Property: " + expEditor.getSelectedProperty().name;
+			expEditor.getSelectedProperty().expression = "";
+			app.endUndoGroup();
+		}
 
 		// show UI
 		if (pal instanceof Window){
@@ -57,22 +59,45 @@ function expEditor()
 
 	this.getSelectedProperty = function (){
 		// $.writeln(app.project.activeItem.selectedLayers[0].selectedProperties[0].name)
+
 	 	if(app.project && app.project.activeItem && app.project.activeItem instanceof CompItem){
 
 	 		if(app.project.activeItem.selectedProperties.length>0){
 	 			if(app.project.activeItem.selectedLayers[0].selectedProperties[0] instanceof Property){
-	 				$.writeln(app.project.activeItem.selectedProperties.length);
-	 				$.writeln(app.project.activeItem.selectedLayers[0].selectedProperties[0].name);
-	 				$.writeln(app.project.activeItem.selectedLayers[0].selectedProperties[0].expression);
+	 				$.writeln("nProps:  " + app.project.activeItem.selectedProperties.length);
+	 				// $.writeln("PropertyType:  " + app.project.activeItem.selectedLayers[0].selectedProperties[0]);
+	 				$.writeln("PropertyName:  " + app.project.activeItem.selectedLayers[0].selectedProperties[0].name);
+	 				$.writeln("PropertyExp:  " + app.project.activeItem.selectedLayers[0].selectedProperties[0].expression);
 	 				return app.project.activeItem.selectedLayers[0].selectedProperties[0];
-	 			}else{
-	 				$.writeln("This is not a valid property");
+	 			}
+	 			else if(app.project.activeItem.selectedLayers[0].selectedProperties[0] instanceof PropertyGroup){
+	 				// $.writeln( "PGROUP SELECTED");
+	 				// // $.writeln("Property:  " + app.project.activeItem.selectedLayers[0].propertyGroup.name);
+	 				// $.writeln("PropertyName:  " + app.project.activeItem.selectedLayers[0].selectedProperties[0].name);
+	 				// $.writeln("numProps: " + app.project.activeItem.selectedLayers[0].selectedProperties[0].numProperties);
+	 				// $.writeln("isSelected: " + app.project.activeItem.selectedLayers[0].selectedProperties[0].property(1).selected);
+	 				// var selProp = app.project.activeItem.selectedLayers[0].selectedProperties[0].property(1).name; // THIS IS CRASHING AE !!!!!!!
+
+	 				var parentProp = app.project.activeItem.selectedLayers[0].selectedProperties[0];
+	 				var nProps = parentProp.numProperties;
+	 				var targetProp;
+
+	 				for (var i=1; i < nProps+1; i++){
+	 					if(parentProp.property(i).selected){
+	 						// $.writeln ("GOTCHA!   " + parentProp.property(i).name);
+	 						targetProp = parentProp.property(i);
+	 					}
+	 				}
+
+	 				return targetProp;
 	 			}
 			}else{
-				$.writeln("Select a Property first");
+				// $.writeln("Select a Property first");
+				alert(this.errNoSelectedProp);
 			}
 		}else{
-			$.writeln("No Composition is selected");
+			// $.writeln("No Composition is selected");
+			alert(this.errNoSelectedComp);
 		}
 	};
 
