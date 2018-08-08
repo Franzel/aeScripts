7fa0fc925d24979fb54febd2d5b1a760a16912d4
Francisco Zamorano <franciscozam@gmail.com>
2018-08-08 14:54:20 -0400

    updates - non functional yet

--- test/test.jsx
+++ test/test.jsx
@@ -1,7 +1,6 @@
-function copyParameter(){
+function parameterValueCloner(){
 
-	var copyParameter = this;
-	var useWidth = true;
+	var parameterValueCloner = this;
 
 	this.buildUI = function(thisObj){
 
@@ -10,38 +9,30 @@ function copyParameter(){
 		//resource specs
 		var res =
 		"group { orientation:'column', alignment:['fill', 'top'], alignChildren:['fill', 'top'], \
-			gr_Actions: Panel { orientation:'column', alignChildren:['fill', 'top'],\
-				fitText: StaticText{text:'Fit to current:'},\
-				fitW: RadioButton { text:'Width', value:true}, \
-				fitH: RadioButton { text:'Height', value:false}, \
-				findBtn: Button { text:'Replace Source', preferredSize:[100,20]}, \
-			},\
+		gr_Actions: Panel { orientation:'column', alignChildren:['fill', 'top'],\
+			copyBtn: Button { text:'Copy Value', preferredSize:[100,20]}, \
+			applyBtn: Button { text:'Apply Value', preferredSize:[100,20]}, \
+		},\
 		}";
 
 		pal.grp = pal.add(res);
 
 		//help tips
-		pal.grp.gr_Actions.findBtn.helpTip = "replace layer source maintaining current scale";
+		pal.grp.gr_Actions.copyBtn.helpTip = "copy parameter";
 		//callbacks
-		pal.grp.gr_Actions.findBtn.onClick = function(){
-			app.beginUndoGroup("Replace Source and Scale");
-			runSelectSource();
+		pal.grp.gr_Actions.copyBtn.onClick = function(){
+			app.beginUndoGroup("Copy Parameter");
+			getMasterProperty();
 			app.endUndoGroup();
 		};
-		pal.grp.gr_Actions.fitW.onClick = function(){
-			if (!(this.value)){
-				useWidth = false;
-			}else{
-				useWidth = true;
-			}
+
+		pal.grp.gr_Actions.applyBtn.onClick = function(){
+			app.beginUndoGroup("Paste Parameter");
+			copyToSelectedLayers();
+			app.endUndoGroup();
 		};
-		pal.grp.gr_Actions.fitH.onClick = function(){
-			if (!(this.value)){
-				useWidth = true;
-			}else{
-				useWidth = false;
-			}
-		};
+
+
 
 		//show UI	
 		if (pal instanceof Window){
@@ -53,48 +44,86 @@ function copyParameter(){
 	};//end buildUI
 
 
-	runCopyParameter =	function(){
+	this.getMasterProperty = function(){
+		if(app.project && app.project.activeItem && app.project.activeItem instanceof CompItem && app.project.activeItem.selectedLayers.length>0){
+			var project= app.project;
+			var selComp = project.activeItem; //get the active comp
 
-	if(app.project && app.project.activeItem && app.project.activeItem instanceof CompItem && app.project.activeItem.selectedLayers.length>0){
-				var project= app.project;
-	var selComp = project.activeItem; //get the active comp
-      
-  var selLayer = selComp.selectedLayers[0];
-  var selEffect = selLayer.selectedProperties[0];
-  var numParameters = selEffect.numProperties;
+			var selLayer = selComp.selectedLayers[0];
+			var selEffect = selLayer.selectedProperties[0];
+			var numParameters = selEffect.numProperties;
 
-  //make sure we clear variables every time before assigning values
-  var selEffectName = null;
-  var selEffectValue = null;
-  var selEffectIndex = null;
+			//make sure we clear variables every time before assigning values
+			var selEffectName = null;
+			var selEffectValue = null;
+			var selEffectIndex = null;
+			var targetProp = null;
 
+			//Get the selected property and parameter
+			//for each selected property, check if any of its parameters is selected =true
+			//if so, copy name of Effect, name of Parameter and its value and save them for later
+			for (var i = 1; i < numParameters +1; i++) {
+				if(selComp.selectedLayers[0].selectedProperties[0].property(i).selected == true){
+					targetProp = selComp.selectedLayers[0].selectedProperties[0].property(i);
+					selEffectName = selEffect.name;
+					selEffectValue = selEffect.property(i).value;
+					selParameterName = selEffect.property(i).name;
+			 	}
+			}
 
-  //Get the selected property and parameter
-  //for each selected property, check if any of its parameters is selected =true
-  //if so, copy name of Effect, name of Parameter and its value and save them for later
-  for (var i = 1; i < numParameters +1; i++) {
-    if(selComp.selectedLayers[0].selectedProperties[0].property(i).selected == true){
-      selEffectName = selEffect.name;
-      selEffectValue = selEffect.property(i).value;
-      selParameterName = selEffect.property(i).name;
-    }
-  }
-
-  alert ("You selected layer " + selEffectName + ", " + selParameterName + "\n" + "Value of: " + selEffectValue + "\n" + "Will be copied to " + app.project.activeItem.selectedLayers.length + " layers");
-
-		}else{
+  			alert ("You selected layer " + selEffectName + ", " + selParameterName + "\n" + "Value of: " + selEffectValue + "\n" + "Will be copied to " + app.project.activeItem.selectedLayers.length + " layers");
+			return targetProp;
+		}
+		else{
 			alert("Please select some layers first");
 		}
+	} //end getMasterProperty
 
-	}
+	this.copyToSelectedLayers = function(){
+		for(var i=0;i<app.project.activeItem.selectedLayers.length;i++){
 
+			$.writeln(" ----------------------------- ");
 
+			var nProps = selComp.selectedLayers[i].property("Effects").numProperties; //get number of effects for this layer
+			$.write("LAYER ");
+			$.writeln(selComp.selectedLayers[i].name);
 
+			for (var j=1; j < nProps+1; j++){
+	      		$.write("   " + selComp.selectedLayers[i].property("Effects").property(j).name);
+	      		var nParameters = selComp.selectedLayers[i].property("Effects").property(j).numProperties; //get number of parameters for each effect
+	      		$.writeln(", contains " + nParameters + " parameters");
+
+				//iterate through all the parameters on this effect
+		      	for (var k = 1; k < nParameters+1; k++){ 
+			      	$.write("       ");
+			      	$.write(selComp.selectedLayers[i].property("Effects").property(j).property(k).name);
+			      	$.writeln(" : " + selComp.selectedLayers[i].property("Effects").property(j).property(k).value);
+
+			        //if name of the effect matches the original selection 
+			        if(selComp.selectedLayers[i].property("Effects").property(j).name == selEffectName){
+			          //if name of the parameter matches the original selection 
+			          	if(selComp.selectedLayers[i].property("Effects").property(j).property(k).name == selParameterName){
+			          		$.writeln(" <-- target"); 
+			            	selComp.selectedLayers[i].property("Effects").property(j).property(1).setValue(getMasterProperty.value); // apply the value of original selection
+			        	}
+			    	}
+				}
+
+				$.writeln(" ---- ");
+			}
+
+			$.writeln(" ----------------------------- ");
+		}//end i for
+	}// end copyToSelectedLayers
 
 	this.run = function(thisObj){
 		this.buildUI(thisObj);
 	};
 
-}//end selectSource (main)
+}//end parameterValueCloner (main)
 
-new copyParameter().run(this);
+new parameterValueCloner().run(this);
+
+
+
+
